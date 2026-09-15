5febe730bfec3d3ce2dfce4e513bcb1596a24072
Jorge <jorge@example.com>
2026-09-15 21:47:57 +0100

    nada importante

--- projects/calc_notas.py
+++ projects/calc_notas.py
@@ -1,8 +1,28 @@
 #gerador de recibo
-
+import datetime as daste 
 #cabecalho
 print("============================================")
 print("                 SUPERJORGE           ")
 print("\n             Freeport outlet       ")
 print("           Super mercado jorge          ")
 print("=============================================")
+
+print(("\nProdutos"))
+lista = {
+    "Chocolate": 2.99,
+    "Manteiga": 3.99,
+    "Farinha": 5.00
+}
+def adicionar_produto():
+    for produto, preco in lista:
+        print(produto, lista)
+    adicionar = input("\nAdicione o produto: ")
+    lista.append(adicionar)
+    print("produto adicionado")
+adicionar_produto()
+adicionar_produto()
+adicionar_produto()
+
+print("\n----Produtos total-------")
+print("\n".join([str(x) for  x in lista]))
+print("A lista total dos produtos: ",len(lista))
--- projects/problems.py
+++ projects/problems.py
@@ -1,6 +1,45 @@
-pi = 3.14159
+#Jogo inutil nao sei pq eu fiz isso.
+import random
 
-raio = 2
-area = pi * raio**2
+opcoes = ["pedra", "papel", "tesoura"]
+primeira_vez = True
+while True:
+    if primeira_vez: 
+        print("Queres Jogar?")
+    else:
+        print("Queres jogar outra vez?? ")
+    play = input("\nIntroduza um opcao(s/n): ").lower()
+    if play == "s":
+        escolha_computador = random.choice(opcoes)
+        primeira_vez = False
+        print("pedra, papel ou tesoura")
+        escolha_user = input("Escolha uma opcao valida: ").lower()
+        
+        if escolha_user not in opcoes:
+            print("Opcao invalida")
+            continue
+        elif escolha_user == escolha_computador:
+            print(f"Voce escolheu: {escolha_user}")
+            print(f"O computador escolheu: {escolha_computador}")
+            print("Empatado")
 
-print(f"A={area:.4f}")
+        #Onde o user sempre ganha
+        elif (escolha_user == "papel" and escolha_computador == "pedra") or \
+            (escolha_user == "pedra" and escolha_computador == "tesoura") or \
+            (escolha_user == "tesoura" and escolha_computador == "papel"):
+            print(f"Voce escolheu: {escolha_user}")
+            print(f"O computador escolheu: {escolha_computador}")
+            print("Voce ganhou")
+        elif (escolha_computador == "papel" and escolha_user == "pedra") or \
+            (escolha_computador == "pedra" and escolha_user == "tesoura") or \
+            (escolha_computador == "tesoura" and escolha_user == "papel"):
+            print(f"Voce escolheu: {escolha_user}")
+            print(f"O computador escolheu: {escolha_computador}")
+            print("Voce perdeu")
+        
+    elif play == "n":
+        print("Ok, tchau")
+        break
+
+    else:
+        print("Opcao invalida, tente outra vez")
